ff39fc0ec18ff3c4c10927baa00b960830771d16
Andrew Stec <andrewstec@gmail.com>
2019-09-12 09:14:08 -0700

    Minor editing of comments

--- exampleFiles/helloWorld.sh
+++ exampleFiles/helloWorld.sh
@@ -18,15 +18,16 @@ echo "\n\n"
 
 ## Requirements:
 
+echo "Requirements: \n"
 echo "#1: I have moved a pre-built 'report.json' into the /output/staff/ directory."
-echo "My pre-built report.json matches the schema: https://github.com/ubccpsc/classy/blob/956e78328c14146e2246b89f1fe0c6e60cb689ed/packages/common/types/ContainerTypes.ts#L69-L106"
+echo "My pre-built report.json matches the schema: https://github.com/ubccpsc/classy/blob/956e78328c14146e2246b89f1fe0c6e60cb689ed/packages/common/types/ContainerTypes.ts#L69-L106\n\n"
 
 ## Nice to Have's:
 
-echo "Nice to Have's:"
+echo "Nice to Have's:\n"
 echo "#1: I have a left-over data from building the report. I suppose that I can produce an HTML coverage report."
 echo "    Anything that I put in the /output/student/ directory will be accessible to the student through an HTTP link."
 
-echo "#2: I want log information accessible to an admin, so I will move it into the /output/admin folder."
+echo "#2: I want a copy of the expected test results, but only the admin should have access to it. I will move it into the /output/admin folder."
 
-echo "#3: I want my TAs to be able to see the original student code and test results at the time of marking, so I have moved a copy into the /output/staff folder."
+echo "#3: I want my TAs to be able to see the full JSON test results at the time of marking, so I have moved a copy into the /output/staff folder."
